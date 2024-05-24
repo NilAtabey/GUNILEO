48b9a681b361ac9e00e629fd730a313cfb04db7c
Leonardo Biason <64312343+ElBi21@users.noreply.github.com>
2024-05-24 12:27:25 +0000

    Added the main notebook and two sample files in the assets

---
+++ assets/cnn.py
@@ -0,0 +1,21 @@
+class LabialCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 5, 3), # 1 is the input (because it's a gray-scale image),
+                                # 5 is the output, 3 is the kernel size
+            nn.ReLU(),
+            nn.Conv2d(5, 10, 3),
+            nn.ReLU()
+        )
+        self.mlp = nn.Sequential(
+            nn.Linear(24 * 24 * 10, 10),
+            nn.ReLU(),
+            nn.Linear(10, 10)
+        )
+
+    def forward(self, x, debug=False):
+        x = self.cnn(x)
+        if debug: print(x.shape)
+        x = torch.flatten(x, 1)
+        if debug: print(x.shape)
---
+++ assets/dataloader.py
@@ -0,0 +1,46 @@
+import os
+import pandas as pd 
+from torchvision.io import read_image
+from torch.utils.data import Dataset
+
+class DataLoader(Dataset):
+    def __init__(self, labels_path: str, images_dir: str, transform = None) -> None:
+        """
+        Creates a dataset given the path to the labels and the image directory
+
+        Parameters:
+            - `labels_path`: the path to the `csv` file containing the labels;
+            - `images_dir`: the path to the directory with the images;
+            - `transform`: states whether a transformation should be applied to the images or not.
+        """
+        super().__init__()
+        self.images_dir = images_dir
+        self.transform = transform
+        self.labels = pd.read_csv(labels_path)
+
+    def __len__(self) -> int:
+        """Returns the length of the dataset
+        
+        Returns:
+            - `length` (`int`): the length of the dataset"""
+        return len(self.labels)
+
+    def __getitem__(self, index: int) -> tuple:
+        """Get the ith item in the dataset
+        
+        Parameters:
+            - `index`: the index of the image that must be retrieven.
+            
+        Returns:
+            - `image` (`img`): the image in the ith position in the dataset."""
+        
+        # Get the images path
+        images_path = os.path.join(self.images_dir, self.labels.iloc[index, 0])
+        image = read_image(images_path) # Can also be done with OpenCV's function cv2.imread()
+        label = self.labels.iloc[index, 1]
+
+        # Apply transformations (if any)
+        if self.transform:
+            image = self.transform(image)
+
+        return (image, label)
