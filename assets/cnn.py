ab8ab3cfd144a223bc845e972b5ad718d28268ef
Leonardo Biason <64312343+ElBi21@users.noreply.github.com>
2024-05-28 15:02:08 +0000

    Main + CNN

--- assets/cnn.py
+++ assets/cnn.py
@@ -4,23 +4,23 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 from torch import nn
 
-class LabialCNN(nn.Module):
+class LabialCNN(nn.Module, debug=False):
     def __init__(self):
         super().__init__()
         self.cnn = nn.Sequential(
-            nn.Conv2d(1, 5, 3),
-            nn.ReLU(),
-            nn.Conv2d(5, 10, 3),
-            nn.ReLU()
-        )
-        self.mlp = nn.Sequential(
-            nn.Linear(24 * 24 * 10, 10),
-            nn.ReLU(),
-            nn.Linear(10, 10)
+            nn.Conv3d(in_channels=1, out_channels=1, kernel_size=(3, 5, 5), padding=(1, 2, 2)),
+            nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2)),
+
+            nn.Conv3d(in_channels=1, out_channels=1, kernel_size=(3, 5, 5), padding=(1, 2, 2)),
+            nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2)),
+            
+            nn.Conv3d(in_channels=1, out_channels=1, kernel_size=(3, 3, 3), padding=(1, 2, 2)),
+            nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
         )
 
-    def forward(self, x, debug=False):
+    # Remember to put FALSE
+    def forward(self, x, debug):
         x = self.cnn(x)
-        if debug: print(x.shape)
+        if debug: print(f"  Layer's shape: {x.shape}")
         x = torch.flatten(x, 1)
-        if debug: print(x.shape)
+        if debug: print(f"  Layer's shape: {x.shape}")
