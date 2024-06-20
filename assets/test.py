500f205d9a45303f1870b004852a5fe60bf79c4d
GnkYzk <gnkyzk1@gmail.com>
2024-06-20 11:13:52 +0200

    Cleaned the dataset from >75 frames

--- assets/test.py
+++ assets/test.py
@@ -1,5 +1,6 @@
-from gnldataloader import GNLDataLoader
-
+#from gnldataloader import GNLDataLoader
+import os
+import cv2
 def main(user: int = 0):
     # Create the dataloaders of our project
     if user == 0:       # Codespace
@@ -14,5 +15,35 @@ def main(user: int = 0):
 
     print(dataLoader[1:3])
 
+def remove_bad_data(delete):
+    path_data = "data/matching/fronts"
+    vidlist= os.listdir(path_data)
+    for vid in vidlist:
+        a= vid.split(".")
+        js = a[0]+".json"
+        
+        pathname = os.path.join(path_data,vid)
+       
+        cap =cv2.VideoCapture(pathname)
+        """ if not cap.isOpened():
+            cap.release()
+            os.remove("data/matching/fronts/"+vid)
+            os.remove("data/matching/labels/"+js) """
+        
+        if int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) > 75:
+            if delete:
+            
+                cap.release()
+                os.remove("data/matching/fronts/"+vid)
+                os.remove("data/matching/labels/"+js)
+            else:
+                
+                print(vid)
+                
+
+def count_all_vids():
+    path_data = "data/matching/fronts"
+    for vid in os.listdir(path_data):
+        print(vid)
 if __name__ == "__main__":
-    main()
+    remove_bad_data(0)
